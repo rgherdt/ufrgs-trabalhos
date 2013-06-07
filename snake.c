2a9fd304a971f7ec9f0becf2c1044501f7eda3cf
ricardogh <rgherdt@inf.ufrgs.br>
2013-06-07 18:44:40 -0300

    Revert "adicionei comentários aos cenários; habilitei comentários em desenhaCenario"
    This reverts commit 55a45a7d54fbf2831a13ae9c6f75f9efc10d3c8e.

--- snake.c
+++ snake.c
@@ -257,7 +257,7 @@ void desenhaCenario(FILE *cenario, struct pos *alimentos)
   alimInd = 0; /* inicializa i para controlar adição de alimentos */
 
   /* Muros */
-  while(fgets(linha, 80, cenario) != NULL)
+  while(fgets(linha, 20, cenario) != NULL)
     {
       if(linha[0] == 'H' || linha[0] == 'V')
 	{
@@ -277,9 +277,6 @@ void desenhaCenario(FILE *cenario, struct pos *alimentos)
 	  alimentos[alimInd].y = y;
 	  alimInd++;
 	}
-      else if
-	(linha[0] == '#') /*ignora comentários. Desnecessário, mas por segurança...*/
-	{}
     }
   wattroff(jogo_win, COLOR_PAIR(3));
   fclose(cenario);
