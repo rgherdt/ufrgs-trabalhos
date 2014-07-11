6b0988fe518d81e6f828ecfe33979512ac36dace
rgherdt <rgherdt@inf.ufrgs.br>
2014-07-11 14:57:33 -0300

    alu funcionando

--- ahmes/ahmes_alu.vhdl
+++ ahmes/ahmes_alu.vhdl
@@ -6,23 +6,28 @@ use work.ahmes_lib.all;
 entity alu is
 port (x : in signed(7 downto 0);
       y : in signed(7 downto 0);
-      alu_opsel : in std_logic_vector(5 downto 0);
+      alu_opsel : in std_logic_vector(9 downto 0);
       alu_out : out signed(7 downto 0);
       flags_out : out std_logic_vector(4 downto 0));
 end alu;
 
 architecture behv of alu is
     signal res : signed(7 downto 0);
-    signal alu_add, alu_or, alu_and, alu_not, alu_py, alu_sub : std_logic;
+    signal alu_add, alu_or, alu_and, alu_not, alu_py, alu_sub,
+           alu_shl, alu_shr, alu_rol, alu_ror : std_logic;
 --    signal flags : std_logic_vector(4 downto 0);
 begin
-    alu_add <= alu_opsel(5);
-    alu_or  <= alu_opsel(4);
-    alu_and <= alu_opsel(3);
-    alu_not <= alu_opsel(2);
-    alu_py  <= alu_opsel(1);
-    alu_sub <= alu_opsel(0);
-
+    alu_add <= alu_opsel(9);
+    alu_or  <= alu_opsel(8);
+    alu_and <= alu_opsel(7);
+    alu_not <= alu_opsel(6);
+    alu_py  <= alu_opsel(5);
+    alu_sub <= alu_opsel(4);
+    alu_shl <= alu_opsel(3);
+    alu_shr <= alu_opsel(2);
+    alu_rol <= alu_opsel(1);
+    alu_ror <= alu_opsel(0);
+        --    nzcvb
 --    flags <= "00000";
 
     alu: process (x, y, alu_add, alu_or, alu_and, alu_not, alu_py, alu_sub)
@@ -45,11 +50,12 @@ begin
             else
                 vflag := '0';
             end if;
+            cflag := temp_res(8);
         elsif (alu_or = '1')  then temp_res := x or y;
         elsif (alu_and = '1') then temp_res := x and y;
         elsif (alu_not = '1') then temp_res := not x;
         elsif (alu_sub = '1') then
-            temp_res := ('0' & x) + (not y) + 1;
+            temp_res := ('0' & x) + ('0' & (not y)) + 1;
             if (x(7) = '0' and y(7) = '1' and temp_res(7) = '1') then
                 vflag := '1';
             elsif (x(7) = '1' and y(7) = '0' and temp_res(7) = '0') then
@@ -57,13 +63,13 @@ begin
             else
                 vflag := '0';
             end if;
+            bflag := not temp_res(8);
         end if;
-        if (std_logic_vector(temp_res) = "000000000") then zflag := '1';
-        elsif (temp_res(7) = '1') then nflag := '1';
+        if (std_logic_vector(temp_res(7 downto 0)) = "00000000") then zflag := '1';
+        elsif (temp_res(7) = '1') then nflag := '1'; zflag := '0';
+        else nflag := '0'; zflag := '0';
         end if;
         alu_out <= temp_res(7 downto 0);
-        cflag := temp_res(8);
-        bflag := not temp_res(8);
         flags_out <= nflag & zflag & cflag & vflag & bflag;
     end process;
 end behv;
--- ahmes/testbench/ahmes_alu_test.vhdl
+++ ahmes/testbench/ahmes_alu_test.vhdl
@@ -17,21 +17,20 @@ architecture test_alu_behv of test_alu is
     component alu is
     port (x : in signed(7 downto 0);
           y : in signed(7 downto 0);
-          alu_opsel : in std_logic_vector(5 downto 0);
+          alu_opsel : in std_logic_vector(9 downto 0);
           alu_out : out signed(7 downto 0);
           flags_out : out std_logic_vector(4 downto 0));
     end component;
 
     signal clock : std_logic;
-    signal sel : std_logic_vector(5 downto 0);
+    signal sel : std_logic_vector(9 downto 0);
     signal flags : std_logic_vector(4 downto 0);
     signal flags_ld : std_logic;
     signal flags_out : std_logic_vector(4 downto 0);
     signal x, y, alu_out : signed(7 downto 0);
 
 begin
-    flags <= "00000";
-    flags_ld <= '0', '1' after 1 ns, '0' after 3 ns;
+    flags_ld <= '0';
     flagsReg : reg5
     port map (reg_in  => flags,
               clk     => clock,
@@ -41,9 +40,9 @@ begin
     clock <= '0', '1' after 1 ns, '0' after 2 ns, '1' after 3 ns, '0' after 4 ns, '1' after 5 ns,
              '0' after 6 ns, '1' after 7 ns, '0' after 8 ns, '1' after 9 ns, '0' after 10 ns, '1' after 11 ns;
     flags <= "00000";
-    x <= "01111111", "00000011" after 5 ns, "00000011" after 10 ns, "00000100" after 15 ns;
+    x <= "00000010";
     y <= "00000011";
-    sel <= "000000", "100000" after 2 ns, "100000" after 4 ns;
+    sel <= "0000010000", "1000000000" after 1 ns, "1000000000" after 4 ns;
     r: alu port map (
 --            clk => clock,
             x => x,
