6c1f016eb19a039bb9d6f3c46612f00e895640f0
rgherdt <rgherdt@inf.ufrgs.br>
2015-06-12 01:20:16 -0300

    renomeei

--- p-median/src/Grasp.hs
+++ p-median/src/Grasp.hs
@@ -3,8 +3,10 @@ module Grasp where
 import Data.List
 import System.Random
 
+type Solution = [Int]
+
 -- | Return a random solution to the problem: a p-size list of locations.
--- The resulting list is already sorted.
-firstSolution :: StdGen -> Int -> Int -> [Int]
-firstSolution gen n p = sort . take p . nub . randomRs (1, n) $ gen
+-- The solution is already sorted for efficiency reasons.
+randomSolution :: StdGen -> Int -> Int -> Solution
+randomSolution gen n p = sort . take p . nub . randomRs (1, n) $ gen
 
