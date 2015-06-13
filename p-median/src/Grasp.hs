c8a3aa8c1a6f105bfb307d92a8bc6754abe93748
Ricardo G. Herdt <rgherdt@inf.ufrgs.br>
2015-06-13 10:53:09 -0300

    neighbours

--- p-median/src/Grasp.hs
+++ p-median/src/Grasp.hs
@@ -1,12 +1,21 @@
 module Grasp where
 
+import Data.Array
 import Data.List
 import System.Random
 
-type Solution = [Int]
+type Solution = Array Int Int
 
 -- | Return a random solution to the problem: a p-size list of locations.
 -- The solution is already sorted for efficiency reasons.
 randomSolution :: StdGen -> Int -> Int -> Solution
-randomSolution gen n p = sort . take p . nub . randomRs (1, n) $ gen
+randomSolution gen n p =
+    listArray (1, p) . sort . take p . nub . randomRs (1, n) $ gen
 
+neighbours :: Int -> Solution -> [Solution]
+neighbours p sol = do
+    pos <- indices sol
+    nbs <- map (\v -> sol // [(pos, v)]) emptyVertices
+    return nbs
+  where
+    emptyVertices = [1 .. p] \\ elems sol
